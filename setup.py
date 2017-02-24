66b097c0fd66d1c82d1e0caf478768017ff734df
Maciej Klimek <maciej.klimek@deepsense.io>
2017-02-24 10:10:43 +0100

    working

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name='mrunner',
     version='0.0.3',
-    py_modules=['mrunner'],
+    packages=['mrunner'],
+    #py_modules=['mrunner'],
     install_requires=['PyYAML'],
     entry_points={
         'console_scripts': [
-            'mrunner=mrunner:main',
+            'mrunner=mrunner.mrunner:main',
         ],
     },
 )
