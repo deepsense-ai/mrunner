344d101e58fbbf84d5c27de1f59031eff1b84d12
Maciej Klimek <maciej.klimek@deepsense.io>
2017-02-27 20:14:35 +0100

    fixed a bug with empty output

--- mrunner/command_gen.py
+++ mrunner/command_gen.py
@@ -48,6 +48,7 @@ def generate_commands(sp):
             repl.append(generate_combination(tmp, ''))
         else:
             repl.append([arg])
+
     argscombination = generate_combination(repl)
     args_modif = generate_combination([x for x in repl if len(x) > 1])
 
@@ -82,6 +83,7 @@ def main():
     except ValueError:
         control_args = []
         proper_args = argv
+
     # print control_args, proper_args
     parser = create_parser()
     args = parser.parse_args(control_args)
@@ -90,9 +92,11 @@ def main():
     commands = my_generate_commands(proper_args, repeat=args.repeat)
     if args.shuffle:
         random.shuffle(commands)
-
     if args.limit is not None:
         commands = commands[:args.limit]
+
+    if len(commands) == 0:
+        commands = [' '.join(argv)]
 
     print '\n'.join(commands)
     return 0
