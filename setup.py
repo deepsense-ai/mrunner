e08ee5a9522825aeb8da004e86be960342a71d19
Maciej Klimek <maciej.klimek@deepsense.io>
2017-03-15 13:42:35 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='mrunner',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     install_requires=['PyYAML'],
     entry_points={
