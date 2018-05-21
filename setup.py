76d1fd5aa82d087b10690703b49a7c383fbd6ff8
Pawel Z <1385347+pawel-ziecina@users.noreply.github.com>
2018-05-21 15:57:06 +0200

    Remove neptune-cli requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mrunner',
-    version='0.1.87',
+    version='0.1.88',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['neptune-cli', 'PyYAML', 'fabric3', 'path.py', 'jinja2', 'six', 'attrs>=17.3', 'click',
+    install_requires=['PyYAML', 'fabric3', 'path.py', 'jinja2', 'six', 'attrs>=17.3', 'click',
                       'docker', 'kubernetes>=5.0.0', 'google-cloud'],
     entry_points={
         'console_scripts': [
