9eb7fd721c7852f0c01abaffe82aa267c95e7082
piotrmilos <pmilos@mimuw.edu.pl>
2019-06-10 08:56:33 +0200

    Remote envs are now setup automatically

--- mrunner/example/basic_setup.sh
+++ mrunner/example/basic_setup.sh
@@ -19,28 +19,6 @@ function prepare_local_venv {
     pip install -r resources/requirements_local.txt
 }
 
-function prepare_remote_venv_prometheus {
-    echo "=================================================================="
-    echo "Setting up remote env on PROMETHEUS in mrunner_example_env"
-    echo "=================================================================="
-    scp resources/requirements_remote.txt $PROMETHEUS_LOGIN@pro.cyfronet.pl:
-    scp resources/setup_remote_env.sh $PROMETHEUS_LOGIN@pro.cyfronet.pl:
-
-    ssh $PROMETHEUS_LOGIN@pro.cyfronet.pl chmod +x setup_remote_env.sh
-    ssh $PROMETHEUS_LOGIN@pro.cyfronet.pl ./setup_remote_env.sh
-}
-
-function prepare_remote_venv_eagle {
-    echo "=================================================================="
-    echo "Setting up remote env on EAGLE in mrunner_example_env"
-    echo "=================================================================="
-    scp resources/requirements_remote.txt $PROMETHEUS_LOGIN@eagle.man.poznan.pl:
-    scp resources/setup_remote_env.sh $PROMETHEUS_LOGIN@eagle.man.poznan.pl:
-
-    ssh $PROMETHEUS_LOGIN@eagle.man.poznan.pl chmod +x setup_remote_env.sh
-    ssh $PROMETHEUS_LOGIN@eagle.man.poznan.pl srun -p plgrid-testing -c 1 -t 00:15:00 ./setup_remote_env.sh
-}
-
 function prepare_mrunner_config {
     echo "=================================================================="
     echo "Preparing mrunner config in /tmp/mrunner_config.yaml"
@@ -59,16 +37,12 @@ function prepare_mrunner_config {
 function prepare_envs_and_mrunner_config {
     if [ -z "$PROMETHEUS_LOGIN" ]; then echo "PROMETHEUS_LOGIN must be set. exiting";exit; fi
        prepare_local_venv
-       prepare_remote_venv_prometheus
-#       prepare_remote_venv_eagle
       prepare_mrunner_config
 }
 
 echo "=================================================================="
-echo "This script will prepare a local virutal environment, remote virtual envrionment on prometheus and simple run config."
-echo "We assume that you have locally installed python3 and access to prometheus"
+echo "This script will prepare a local virtual environment and simple run config."
 echo "Please review this script before running it."
-
 echo "=================================================================="
 
 echo "Should we run?"
