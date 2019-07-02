c5b74dbf092b557499f544d8bd64e13729f3c5d5
KC <konrad.czechowski@gmail.com>
2019-07-02 17:34:24 +0200

    Client helper: Add pwd property with path to experiment, add specification filename to tags.

--- mrunner/helpers/client_helper.py
+++ mrunner/helpers/client_helper.py
@@ -9,6 +9,27 @@ import cloudpickle
 neptune_logger_on = False
 
 
+def neptune_set_property(key, value):
+  """Set property, cast value to str if needed
+
+  Note that neptune.set_property() might raise exception but also can
+  fail silently.
+  """
+  import neptune
+  def set_property_assert_value(key, value):
+    neptune.set_property(key, value)
+    assert neptune.get_experiment().get_properties()[key] == value
+
+  try:
+    set_property_assert_value(key, value)
+  except:
+    try:
+      set_property_assert_value(key, str(value))
+    except:
+      neptune.set_property(
+        name, "Not possible to send to neptune. Implement __str__"
+      )
+
 def get_configuration(print_diagnostics=False, with_neptune=False, inject_parameters_to_gin=False):
   global neptune_logger_on
 
@@ -19,10 +40,12 @@ def get_configuration(print_diagnostics=False, with_neptune=False, inject_parame
 
   params = None
   experiment = None
+  spec_filename = None
 
   if commandline_args.ex:
     from path import Path
-    vars = {'script': str(Path(commandline_args.ex).name)}
+    spec_filename = str(Path(commandline_args.ex).name)
+    vars = {'script': spec_filename}  # Do we need it?
     exec(open(commandline_args.ex).read(), vars)
     experiments = vars['experiments_list']
     print("The specifcation file contains {} "
@@ -51,31 +74,21 @@ def get_configuration(print_diagnostics=False, with_neptune=False, inject_parame
     else:
       neptune_logger_on = True
       import neptune
-
-      def neptune_set_property(key, value):
-        """Set property, cast value to str if needed
-
-        Note that neptune.set_property() might raise exception but also can
-        fail silently.
-        """
-        def set_property_assert_value(key, value):
-          neptune.set_property(key, value)
-          assert neptune.get_experiment().get_properties()[key] == value
-        try:
-          set_property_assert_value(key, value)
-        except:
-          try:
-            set_property_assert_value(key, str(value))
-          except:
-            neptune.set_property(
-              name, "Not possible to send to neptune. Implement __str__"
-            )
-
       neptune.init(project_qualified_name=experiment.project)
       neptune.create_experiment(name=experiment.name, tags=experiment.tags)
       for name in params:
         neptune_set_property(name, params[name])
 
+      # Set pwd property with path to experiment.
+      neptune_set_property("pwd", os.getcwd())
+      # Add specification filename to tags if present (might be added e.g by
+      # mrunner or --ex parameter in local run without mrunner)
+      if "script" in globals():
+        spec_filename = globals()["script"]
+      if spec_filename:
+        neptune.get_experiment().append_tag(
+          spec_filename[:-3]  # remove ".py"
+        )
       import atexit
       atexit.register(neptune.stop)
 
