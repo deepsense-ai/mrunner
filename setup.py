601ce541a8489225dbe7f8852d71cc031869700b
piotrmilos <pmilos@mimuw.edu.pl>
2019-08-03 23:32:40 +0200

    removing warnings

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mrunner',
-    version='0.2.5',
+    version='0.2.4',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['PyYAML', 'fabric3', 'path.py', 'jinja2', 'six', 'attrs>=17.3', 'click',
+    install_requires=['cryptography==2.2.2', 'PyYAML', 'fabric3', 'path.py', 'jinja2', 'six', 'attrs>=17.3', 'click',
                       'docker', 'kubernetes>=5.0.0', 'google-cloud', 'termcolor'],
     entry_points={
         'console_scripts': [
